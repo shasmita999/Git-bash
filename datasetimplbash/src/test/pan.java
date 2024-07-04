87bb3969c5e95e7e7604aa8f2585d6387ba22269
Shasmita Swain <shasmita.swain@prod.hclpnp.com>
2024-07-05 00:19:45 +0530

    commit the files in another branc

---
+++ datasetimplbash/src/test/pan.java
@@ -0,0 +1,24 @@
+package test;
+
+import com.ibm.rational.test.lt.kernel.services.ITestExecutionServices;
+
+/**
+ * @author unknown
+ */
+public class pan implements com.ibm.rational.test.lt.kernel.custom.ICustomCode2 {
+
+	/**
+	 * Instances of this will be created using the no-arg constructor.
+	 */
+	public pan() {
+	}
+
+	/**
+	 * For javadoc of ICustomCode2 and ITestExecutionServices interfaces, select 'Help Contents' in the
+	 * Help menu and select 'Extending Rational Performance Tester functionality' -> 'Extending test execution with custom code'
+	 */
+	public String exec(ITestExecutionServices tes, String[] args) {
+		return null;
+	}
+
+}
